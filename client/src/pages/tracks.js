02b8dafcaf55937f461961143229ee08d050aedd
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2024-01-18 07:54:04 +0500

    basic GraphQL Api Implementation

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React from "react";
 import { Layout } from '../components';
+import { useQuery, gql } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+import QueryResult from "../components/query-result";
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+const TRACKS = gql`
+query GetTracks {
+  trackForHome {
+    id
+    title
+    author {
+      id
+      name
+      photo
+    }
+    thumbnail
+    length
+    modulesCount
+  }
+}
+`;
+
+
+
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+   
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.trackForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
